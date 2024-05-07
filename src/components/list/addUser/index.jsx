3470a3c83231cd60c91ca3f65f04fb0276f021db
junhyeongchae <jchae7184@gmail.com>
2024-05-07 11:30:37 +0900

    user name 검색기능 추가

--- src/components/list/addUser/index.jsx
+++ src/components/list/addUser/index.jsx
@@ -1,7 +1,28 @@
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { useState } from "react";
+import { db } from "../../../lib/firebase";
+
 const AddUser = () => {
+  const [user, setUser] = useState(null);
+
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get("username");
+    try {
+      const userRef = collection(db, "users");
+      const q = query(userRef, where("username", "==", username));
+      const querySnapShot = await getDocs(q);
+      if (!querySnapShot.empty) {
+        setUser(querySnapShot.docs[0].data());
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className="w-max h-max p-7 bg-[rgba(17,25,40,0.8)] rounded-lg absolute inset-0 m-auto">
-      <form className="flex gap-5">
+      <form className="flex gap-5" onSubmit={handleSearch}>
         <input
           type="text"
           placeholder="Username"
@@ -12,19 +33,21 @@ const AddUser = () => {
           Search
         </button>
       </form>
-      <div className="mt-10 flex items-center justify-between">
-        <div className="flex items-center gap-5 text-sm">
-          <img
-            src="./avatar.png"
-            alt="avatar"
-            className="size-12 rounded-full object-cover"
-          />
-          <span>Develsoph1111er</span>
+      {user && (
+        <div className="mt-10 flex items-center justify-between">
+          <div className="flex items-center gap-5 text-sm">
+            <img
+              src={user.avatar || "./avatar.png"}
+              alt="avatar"
+              className="size-12 rounded-full object-cover"
+            />
+            <span>{user.username}</span>
+          </div>
+          <button className="p-2.5 border-none outline-none bg-[#ce3c3cb7] text-white rounded-lg placeholder:text-slate-300 text-xs">
+            Add User11
+          </button>
         </div>
-        <button className="p-2.5 border-none outline-none bg-[#ce3c3cb7] text-white rounded-lg placeholder:text-slate-300 text-xs">
-          Add User11
-        </button>
-      </div>
+      )}
     </div>
   );
 };
